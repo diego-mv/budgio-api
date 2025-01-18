b984486f58301b7f3cba0e9c680bbdfc35d30679
Diego-MV <diegomoralesval@gmail.com>
2025-01-17 23:14:55 -0300

    feat: :sparkles: changes cookies

--- src/domain/use-cases/auth/callback.uc.ts
+++ src/domain/use-cases/auth/callback.uc.ts
@@ -13,18 +13,18 @@ export class CallbackUseCase implements IAuthUseCase.ICallbackUseCase {
 		}
 		res.cookie('access_token', data.accessToken, {
 			httpOnly: false,
-			secure: false,
-			sameSite: 'lax'
+			secure: true,
+			sameSite: 'none'
 		})
 		res.cookie('refresh_token', data.refreshToken, {
 			httpOnly: false,
-			secure: false,
-			sameSite: 'lax'
+			secure: true,
+			sameSite: 'none'
 		})
 		res.cookie('user', JSON.stringify(data.user), {
 			httpOnly: false,
-			secure: false,
-			sameSite: 'lax'
+			secure: true,
+			sameSite: 'none'
 		})
 
 		return res.redirect(`${CONSTANTS.ENV.CLIENT_HOST}/auth/callback`)
